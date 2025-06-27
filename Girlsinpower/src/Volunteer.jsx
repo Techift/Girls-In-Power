f87cfd244c0cbc36454b1adb1fe66bde6ccab334
techift <giftessang6@gmail.com>
2025-06-27 01:45:45 +0100

    i added a vercel.json file

--- Girlsinpower/src/Volunteer.jsx
+++ Girlsinpower/src/Volunteer.jsx
@@ -53,15 +53,31 @@ function Volunteer() {
         className="volunteer_goals"
         style={{
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "center",
           alignItems: "center",
-          padding: "0 4rem",
-          marginTop: "3rem",
+          padding: "0 2rem",
+          marginTop: "4rem",
           flexWrap: "wrap",
+          height: "auto",
+          maxWidth: "1400px",
+          width:'100%',
+          margin:'0 auto',
+          gap:'5rem'
         }}
       >
-        <div className="volunteer_passion" style={{background:'rgb(241, 6, 84)',marginBottom:'2rem', padding:'1rem 2rem', borderRadius:'20px', color:'white',width:'400px',height:'200px'}}>
-          <h3 style={{ textAlign: "center", color: 'white' }}>
+        <div
+          className="volunteer_passion"
+          style={{
+            background: "rgb(241, 6, 84)",
+            marginBottom: "2rem",
+            padding: "1rem 1rem",
+            borderRadius: "20px",
+            color: "white",
+            width: "400px",
+            height: "auto",
+          }}
+        >
+          <h3 style={{ textAlign: "center", color: "white" }}>
             What we are looking for
           </h3>
           <ul style={{ listStyle: "none" }}>
@@ -73,41 +89,77 @@ function Volunteer() {
           </ul>
         </div>
 
-        <div className="volunteer_roles" style={{background:'rgb(12, 82, 14)',marginBottom:'2rem', padding:'1rem 2rem', borderRadius:'20px', color:'white',width:'400px',height:'200px'}}>
+        <div
+          className="volunteer_roles"
+          style={{
+            background: "rgb(12, 82, 14)",
+            marginBottom: "2rem",
+            padding: "1rem 2rem",
+            borderRadius: "20px",
+            color: "white",
+            width: "400px",
+            height: "auto",
+          }}
+        >
           <ul style={{ listStyle: "none" }}>
-            <h3 style={{ textAlign: "center", color: 'white' }}>
+            <h3 style={{ textAlign: "center", color: "white" }}>
               Volunteer roles you can join
             </h3>
             <li>1. Mentorship & Coaching</li>
             <li>2. Graphic Design / Content Creation</li>
             <li>3. Fundraising & Partnership Team</li>
+            <li>4. Bootcamp Support (Tech, Skills, Workshops)</li>
           </ul>
         </div>
       </div>
 
-      <div className="teer_contact" style={{textAlign:'center',padding:'2rem 0'}}>
+      <div
+        className="teer_contact"
+        style={{ textAlign: "center", padding: "2rem 0" }}
+      >
         <h2>Ready to make a difference?</h2>
-        <p style={{fontSize:'1.2rem',marginBottom:'2rem'}}>Fill out our volunteer form and we'll connect you to a role that fits your passion and skills.</p>
-      
+        <p style={{ fontSize: "1.2rem", marginBottom: "2rem" }}>
+          Fill out our volunteer form and we'll connect you to a role that fits
+          your passion and skills.
+        </p>
 
-      <div className="teer_btn">
-                    <Link to="/contact" className={({isActive}) => isActive ? 'active' : ''}>
-        <button style={{ width: '250px',
-  height: 'auto',
-  borderRadius: '20px',
-  border: 'none',
-  backgroundColor: 'rgb(241, 6, 84)',
-  color: 'white',
-  fontSize: '1.2rem',
-  boxShadow: '0 5px 6px rgba(13, 13, 13, 0.2)',
-  margin: '0',
-  padding: '1rem'
-  }}>Become a volunteer</button>
-  </Link>
-      </div>
+        <div className="teer_btn">
+          <Link
+            to="/contact"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            <button
+              style={{
+                width: "250px",
+                height: "auto",
+                borderRadius: "20px",
+                border: "none",
+                backgroundColor: "rgb(241, 6, 84)",
+                color: "white",
+                fontSize: "1.2rem",
+                boxShadow: "0 5px 6px rgba(13, 13, 13, 0.2)",
+                margin: "0",
+                padding: "1rem",
+              }}
+            >
+              Become a volunteer
+            </button>
+          </Link>
+        </div>
       </div>
 
-      <p style={{textAlign:'center',fontStyle:'italic',fontWeight:'500',fontSize:'1.2rem',color:'rgb(241, 6, 84)',marginBottom:'1rem'}}>"Together, let’s build a world where every girl knows her power."</p>
+      <p
+        style={{
+          textAlign: "center",
+          fontStyle: "italic",
+          fontWeight: "500",
+          fontSize: "1.2rem",
+          color: "rgb(241, 6, 84)",
+          marginBottom: "1rem",
+        }}
+      >
+        "Together, let’s build a world where every girl knows her power."
+      </p>
     </>
   );
 }
